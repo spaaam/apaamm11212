907018d77197097428c160237eaa10ee17016876
spaaam <53124531+spaaam@users.noreply.github.com>
2019-07-22 14:18:47 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("522757215988219933")
+setInterval(function() {
+channel.send(`Credit Spam , Frame , Credit By Akeel, Farm Credit By Akeel, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 129 " ]`);
+}, 30)
+})
+
+client.login("NDYxNDkzOTk5NzgyNDYxNDQy.XTQyZg.jabH_ueJGv_-NTS6ac1MDmVIILY");
